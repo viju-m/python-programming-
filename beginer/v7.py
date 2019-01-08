0ada8c52c30ce6abd0e89ba94f477e1ef633469f
viju-m <42570591+viju-m@users.noreply.github.com>
2019-01-08 15:17:46 +0530

    Update v7.py

--- beginer/v7.py
+++ beginer/v7.py
@@ -2,6 +2,6 @@ my_str = 'aIbohPhoBiA'
 my_str = my_str.casefold()
 rev_str = reversed(my_str)
 if list(my_str) == list(rev_str):
-   print("It is palindrome")
+  print("It is palindrome")
 else:
-   print("It is not palindrome")
+  print("It is not palindrome")
