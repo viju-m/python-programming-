36fb10463eebcfea7e22c1987443a412a2a98fcb
viju-m <42570591+viju-m@users.noreply.github.com>
2019-01-08 15:22:39 +0530

    Update v8.py

--- beginer/v8.py
+++ beginer/v8.py
@@ -1,24 +1,24 @@
 int Nth_of_AP(int a, int d, int N) 
 {  
-    // using formula to find the  
+    
     // Nth term t(n) = a(1) + (n-1)*d 
     return (a + (N - 1) * d); 
       
 } 
   
-// Driver code 
+
 int main()  
 { 
     // starting number 
     int a = 2;  
       
-    // Common difference 
+   
     int d = 1;  
       
     // N th term to be find 
     int N = 5;  
       
-    // Display the output 
+   
     cout << "The "<< N  
          <<"th term of the series is : "
          << Nth_of_AP(a,d,N); 
