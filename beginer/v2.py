390384e110fef67e0f188fb7a9646517571b1c90
viju-m <42570591+viju-m@users.noreply.github.com>
2019-01-08 14:42:32 +0530

    Add files via upload

---
+++ beginer/v2.py
@@ -0,0 +1,27 @@
+int main() 
+{ 
+    cout << "First Test\n"; 
+    int arr1[] = {4, 5, 6, 7, 8, 4, 4}; 
+    int size = sizeof(arr1)/sizeof(arr1[0]); 
+    int k = 3; 
+    moreThanNdK(arr1, size, k); 
+  
+    cout << "\nSecond Test\n"; 
+    int arr2[] = {4, 2, 2, 7}; 
+    size = sizeof(arr2)/sizeof(arr2[0]); 
+    k = 3; 
+    moreThanNdK(arr2, size, k); 
+  
+    cout << "\nThird Test\n"; 
+    int arr3[] = {2, 7, 2}; 
+    size = sizeof(arr3)/sizeof(arr3[0]); 
+    k = 2; 
+    moreThanNdK(arr3, size, k); 
+  
+    cout << "\nFourth Test\n"; 
+    int arr4[] = {2, 3, 3, 2}; 
+    size = sizeof(arr4)/sizeof(arr4[0]); 
+    k = 3; 
+    moreThanNdK(arr4, size, k); 
+  
+    return
