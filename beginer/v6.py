cc5c10fc9ac5d9a22499718abf36c23e19e4a2dd
viju-m <42570591+viju-m@users.noreply.github.com>
2019-01-08 15:11:46 +0530

    Add files via upload

---
+++ beginer/v6.py
@@ -0,0 +1,16 @@
+def counting_numbers():
+    i = 1
+    while True:
+        yield i
+        i += 1
+def check_power(N, k):
+    if N <= 0 or k <= 0:
+        raise ValueError("N and k should be greater than 0")
+    if k == 1:  
+        return N == 1 
+    for i in counting_numbers():
+        x = k ** i
+        if x == N :
+            return True
+        elif x > N:
+            return False
