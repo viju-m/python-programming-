44018ae60290c9cc7fc9ef6c0b49636922d84566
viju-m <42570591+viju-m@users.noreply.github.com>
2019-01-08 15:05:55 +0530

    Update v5.py

--- beginer/v5.py
+++ beginer/v5.py
@@ -1,6 +1,10 @@
-nums = []
-n = int(input("How many numbers you want to input? "))
-for i in range(n):
-    nums.append(int(input(">")))
-print("Max: %d\nMin: %d" % (max(nums), min(nums)))
-
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: ")) 
+if (num1 > num2) and (num1 > num3):
+   largest = num1
+elif (num2 > num1) and (num2 > num3):
+   largest = num2
+else:
+   largest = num3
+print("The largest number is",largest)
